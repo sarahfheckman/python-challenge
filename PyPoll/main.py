d3468b370ba0845c2ec0dfdf55419160411f4b22
SarahHeck <sfheckman@gmail.com>
2018-12-06 14:57:16 -0600

    saving changes, cleaning code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,27 +3,35 @@ import os
 import csv
 
 #create path
-pypollCSV = os.path.join("C:/Users/sarah/Desktop/SMDA201811DATA2-master/02-Homework/03-Python/Instructions/PyPoll/Resources/election_data.csv")
+pypollCSV = os.path.join("C:/Users/sarah/Documents/BOOTCAMP/SMDA201811DATA2-master/02-Homework/03-Python/Instructions/PyPoll/Resources/election_data.csv")
+
 
 #set lists
 voters = []
 candidates = []
-votes1 = 0
-votes2 = 0
-votes3 = 0
-votes4 = 0
 
 #open path & pull list values
 with open(pypollCSV, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     #skip the header
     csv_header = next(csvfile)
-    #add values to list
+    #add values to lists
     for column in csvreader:
         candidates.append(column[2])
+        voters.append(column[0])
 
 #pull unique candidates
 candidates_set = set(candidates)
 print(candidates_set)
 
-#---------------------------------1st commit        
+#---------------------------------1st commit 
+
+#set voter variables
+total_voters = len(voters)
+print(total_voters)
+
+with open(pypollCSV, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    #skip the header
+    csv_header = next(csvfile)
+    #determines        
